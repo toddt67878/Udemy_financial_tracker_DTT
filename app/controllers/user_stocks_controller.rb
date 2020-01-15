753553e0e8d590238707bab94d7dc4553b1211e3
Todd Traylor <toddt67878@yahoo.com>
2020-01-15 16:26:31 -0600

    Add stock tracking removal functionality

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,4 +1,4 @@
-class UserStocksController < ApplicationController
+cclass UserStocksController < ApplicationController
 
   def create
     stock = Stock.check_db(params[:ticker])
@@ -8,6 +8,14 @@ class UserStocksController < ApplicationController
     end  
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
     redirect_to my_portfolio_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
